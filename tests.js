0ea5452552acd94df2cdf7ae09b350a178bf3cab
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-07-28 15:41:50 -0500

    add fourth test and solution for intro-to-testing.

--- tests.js
+++ tests.js
@@ -21,8 +21,11 @@ describe("sayHello", function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
+    it('should return the string "Hello, Jane!" when executed', function() {
         expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex! when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 
 });
